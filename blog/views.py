4ac0a1f867d6942eed8741ff476e8c2b87303c4d
glendasp <glendasp@gmail.com>
2016-10-13 19:18:48 -0500

    My first try blog and first commit

---
+++ blog/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+
+# created a function (def) called post_list that takes request and
+# return a function render that will render (put together) our
+# template blog/post_list.html
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
